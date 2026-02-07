23dbc5993ca19362487fe98e73020f6b410bddcf
emsibt <tranthanhbao0309@gmail.com>
2026-02-07 16:59:27 +0700

    add two style of dfs

--- Leetcode/Template/binary_tree_traversals.py
+++ Leetcode/Template/binary_tree_traversals.py
@@ -17,6 +17,27 @@ def postorder(root: TreeNote) -> None:
     postorder(root.right)
     print(root.val)
 
+def dfs_top_down(node, param):
+    if not node:
+        return True
+
+    if not check_logic(node, param):
+        return False
+
+    new_param_left = update(param, node.val)
+    new_param_right = update(param, node.val)
+    return dfs_top_down(node.left, new_param_left) and dfs_top_down(node.right, new_param_right)
+
+def dfs_bottom_up(node):
+    if not node:
+        return 0
+    
+    left_result = dfs_bottom_up(node.left)
+    right_result = dfs_bottom_up(node.right)
+
+    my_res = process(node.val, left_result, right_result)
+    return my_res
+
 # BFS - deque
 from collections import deque
 
